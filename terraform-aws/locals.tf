6f1eee4f9c00994c97801babbdcf73396c33c85c
DanHenrique <danielhenrique.al@outlook.com>
2026-04-20 00:11:40 -0300

    feat: alterando modulo ddb

--- terraform-aws/locals.tf
+++ terraform-aws/locals.tf
@@ -1,8 +1,16 @@
 locals {
   table_name = "orders"
 
+  iam_prefix = "sthore"
+
+  template_variables = {
+    account_id  = data.aws_caller_identity.current.account_id
+    environment = var.environment
+    table_name  = local.table_name
+  }
+
   tags = {
-    ManagedBy   = "terraform"
-    Repository  = "github.com/SthoreH/infra-aws-ddb-orders"
+    ManagedBy  = "terraform"
+    Repository = "github.com/SthoreH/infra-aws-ddb-orders"
   }
 }
